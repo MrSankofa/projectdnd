f137e64ce980874b2c2f7b19ff11f7e6c4bf85ff
Brett Cunningham <bwcunninghamii@gmail.com>
2017-06-22 09:31:31 -0400

    concat the js files to dev js folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp'),
 coffee = require('gulp-coffee'),
+concat = require('gulp-concat'),
 gutil = require('gulp-util');
 
 var coffeeSources = ['/components/coffee/*.coffee'];
+var jsSources = ['/components/scripts/*.js'];
+
+// This task takes all of your js code in your 
+// scripts folder and concatenates them then
+// moves them to the dev/js folder
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
 
 // this task converts our coffee script code and
 // converts it to js. Then it sends it to the scripts
