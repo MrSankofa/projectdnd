b8bae98c3346e83ce80927b79ba9cdec90687da2
Brett Cunningham <bwcunninghamii@gmail.com>
2017-06-22 09:15:21 -0400

    npm install gulp-coffee and created the task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp'),
+coffee = require('gulp-coffee'),
 gutil = require('gulp-util');
+
+var coffeeSources = ['/components/coffee/*.coffee'];
+
+// this task converts our coffee script code and
+// converts it to js. Then it sends it to the scripts
+// folder
+gulp.task('coffee', function () {
+    gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+        .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
+
 gulp.task('log', function() {
 gutil.log('Workflows are awesome');
 });
