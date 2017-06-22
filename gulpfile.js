2de8c3f866e7d57da48516f73a01c7ace9ea92ea
Brett Cunningham <bwcunninghamii@gmail.com>
2017-06-22 10:10:09 -0400

    npm install sass and compass to move/convert scss to css in dev folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp'),
 coffee = require('gulp-coffee'),
 concat = require('gulp-concat'),
+browserify = require('gulp-browserify'),
+compass = require('gulp-compass'),
 gutil = require('gulp-util');
 
-var coffeeSources = ['/components/coffee/*.coffee'];
+var sassSources = ['/components/sass/style.scss'];
 var jsSources = ['/components/scripts/*.js'];
+var coffeeSources = ['/components/coffee/*.coffee'];
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+           sass: 'components/sass',
+           image: 'builds/development/images',
+           style: 'expanded'
+        })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
+});
 
 // This task takes all of your js code in your 
 // scripts folder and concatenates them then
@@ -12,6 +26,7 @@ var jsSources = ['/components/scripts/*.js'];
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify()) //this pipes in our libraries like jquery and mustache
         .pipe(gulp.dest('builds/development/js'))
 });
 
@@ -25,6 +40,9 @@ gulp.task('coffee', function () {
     .pipe(gulp.dest('components/scripts'))
 });
 
+
+// gutil allows you to post things in the terminal to
+// troubleshoot how things are working
 gulp.task('log', function() {
 gutil.log('Workflows are awesome');
 });
