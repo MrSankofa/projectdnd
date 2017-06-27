b87c8fa27e0f11d68d28eb532288d49f78b668d1
Brett Cunningham <bwcunninghamii@gmail.com>
2017-06-26 20:09:19 -0400

    sticky header construction and footer

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -101,7 +101,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('Driving To Be The Best!');
+})('The most creative minds in Art');
 
 fill;
 
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -4,6 +4,6 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('Driving To Be The Best!');
+})('The most creative minds in Art');
 
 fill;
